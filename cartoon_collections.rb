237e86cdabf3216d41030b1e159b74b65fcdc2e9
Maysoon Tabali <mtab03@me.com>
2020-07-23 20:28:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   #planeteer_calls = 0 
-  planeteer_calls.map(&:upcase) do |element|
+  planeteer_calls.map do |element|
+    element
   end 
 end 
     
