dc0d8647450fc9eacdb8f80358a0ce15fc04e081
Maysoon Tabali <mtab03@me.com>
2020-07-23 21:19:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,9 +29,8 @@ def long_planeteer_calls(planeteer_calls)
 
 
 def find_valid_calls(planeteer_calls)
-  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"].include? 
-  valid_calls.find 
-  #include? {|call| call.find}
+  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  planeteer_calls.find {|call| valid_calls.include?(call)}
 end 
 
 #.include?
