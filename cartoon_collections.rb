beca06a2069de40987efbed0feed70e88be37546
Maysoon Tabali <mtab03@me.com>
2020-07-23 21:11:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,8 +30,10 @@ def long_planeteer_calls(planeteer_calls)
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  
+  valid_calls.include? 
 end 
+
+.include?
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
 
